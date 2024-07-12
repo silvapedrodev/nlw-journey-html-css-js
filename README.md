9f1274498b32aa4c16b0608181e74d515a5aaafb
Pedro Silva <pedropgd981@gmail.com>
2024-07-12 17:15:07 -0300

    add modules

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# nlw-journey-js
- Aplicação desenvolvida durante o NLW Journey da Rocketseat.
+<p align="center">
+  <img alt="Logo NLW Journey - Rocketseat" src=".github/logo.png" width="200px" />
+</p>
+
+<p align="center">
+Aplicação desenvolvida no NLW Journey da Rocketseat na trilha HTML+CSS+JS.
+</p>
+
+<h3 align="center">Status: 🛠️ In development</h3>
+
+<p  align="center">
+  <img src="https://img.shields.io/badge/-HTML-05122A?style=flat&logo=html5"></img>
+  <img src="https://img.shields.io/badge/-CSS-05122A?style=flat&logo=css3"></img>
+  <img src="https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript"></img>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=18181B&labelColor=BEF264">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Preview do projeto desenvolvido." src=".github/preview.png" width="60%">
+</p>
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+## Projeto
+
+Nesse projeto iremos desenvolver uma versão simplificada de um sistema de roteiro de viagem!
---
+++ js/date.js
@@ -0,0 +1,67 @@
+
+ 
+ export default function Dates () {
+
+  const formatter = (date) => {
+    return {
+     day: {
+       numeric: dayjs(date).format('DD'),
+       week: {
+         short: dayjs(date).format('ddd'),
+         long: dayjs(date).format('dddd'),
+       }
+     },
+     month: dayjs(date).format('MMMM'),
+     hours: dayjs(date).format('HH:mm')
+   
+    }
+   }
+
+  const createSelectionDays = () => {
+    const days = [
+      "2024-12-27",
+      "2024-12-28",
+      "2024-12-29",
+      "2024-12-30",
+      "2024-12-31",
+    ]
+  
+    let selectionDays = ''
+  
+    for(let day of days) {
+      const format = formatter(day)
+      const formattedDay = `
+      ${format.day.numeric} de
+      ${format.month}
+      `
+      selectionDays += `
+        <option value=${day}>${formattedDay}</option>
+      `
+    }
+  
+    document.querySelector('select[name="day"]').innerHTML = selectionDays
+  }
+  createSelectionDays()
+
+
+  const createSelectionHours = () => {
+    let hoursAvailable = ''
+  
+    for(let i = 6; i < 23; i++) {
+      const hour = String(i).padStart(2, '0')
+      hoursAvailable += `<option value="${hour}:00">${hour}:00</option>`
+      hoursAvailable += `<option value="${hour}:30">${hour}:30</option>`
+    }
+  
+    document.querySelector('select[name="hour"]').innerHTML = hoursAvailable
+  }
+  createSelectionHours()
+
+
+  return {
+    formatter,
+    createSelectionDays,
+    createSelectionHours
+  }
+ }
+ 
